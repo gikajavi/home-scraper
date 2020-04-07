ca660acc62515aaf424d99d2bf68d72c3c7028dd
betterlucknt <betterlucknt@users.noreply.github.com>
2020-04-08 01:17:57 +0200

    Update habitaclia
    He modificat el codi per a: 
                                               - Que guardi les dades en un dataframe de pandas(sera mes facil de fer servir la info despres).
                                               - Fer servir la llibreria de python per fer logs
                                               - Evitar posar valors escrits quan poden ser booleans(Si/No han passat a ser True/False i '?' son ara None) fan que filtrar les dades sigui mes rapid.
                                               - Afegir un retard aleatori(ara mateix de fins a 3 segons) entre crida i crida.
    Tin el codi corrent en local, a veure si em dona algun problema. T'aviso al mati si es aixi.
    
    * Faltaria poder passar com a variables la poblacio i si busquem lloguer o venta.

--- src/Habitaclia.py
+++ src/Habitaclia.py
@@ -1,30 +1,22 @@
+import pandas as pd
+from time import sleep
+import logging
 import random
+import requests
 from bs4 import BeautifulSoup
-from log_helper import LogHelper
-from http_helper import HttpHelper
 
 
 class HabitacliaScraper():
     def __init__(self):
-        self.url = "https://www.habitaclia.com/alquiler-barcelona.htm"
+        self.url = "https://www.habitaclia.com/alquiler-en-garraf.htm"
         self.dataset = []
         self.session_id = "sess-" + str(random.random())
-        self._log_helper = LogHelper("./logs/log-" + self.session_id)
-        self._http_helper = HttpHelper(self._log_helper, 5, 2)
-        self._log("Habitaclia Scraper Iniciado")
+        logging.basicConfig(filename='log.log', filemode='w', format='%(asctime)-15s - %(name)s - %(levelname)s - %(message)s'format=self._FORMAT)
+        self._logger = logging.getLogger('scrapper')
+        self._logger.info("Habitaclia Scraper Iniciado")
 
     def start(self):
-        html_code = self._descargar_indice()
-        self.total_index_pages = int(self._get_total_index_pages(html_code))
-        # Scraping del índice principal:
-        url_to_scrap = self.get_url_to_scrap_by_index(0)
-        self.scrap_index_page(url_to_scrap)
-        # Scraping para el resto de índices (según la paginación)
-        i = 1
-        while i < self.total_index_pages:
-            url_to_scrap = self.get_url_to_scrap_by_index(i)
-            self.scrap_index_page(url_to_scrap)
-            i += 1;
+        self._data = pd.concat([self.scrap_index_page(self.get_url_to_scrap_by_index(i)) for i in range(0,int(self._get_total_index_pages(self._descargar_indice())))])
 
     def get_url_to_scrap_by_index(self, index):
         if index == 0:
@@ -35,24 +27,23 @@ class HabitacliaScraper():
 
     def scrap_index_page(self, url):
         try:
-            self._log("Index scraping en " + url)
+            self._logger.info("Index scraping en " + url)
             html = self._descargar_url(url)
             parser = BeautifulSoup(html, 'html.parser')
             links = parser.select(".list-item-title a")
-            for link in links:
-                data_url = link['href'];
-                self.scrap_data_page(data_url)
+            return(pd.concat(self.scrap_data_page(link['href'])for link in links))
+                
         except Exception as ex:
-            self._log("Error en el index scraping " + str(ex))
+            self._logger.error("Error en el index scraping " + str(ex))
 
     def scrap_data_page(self, url):
         try:
-            self._log("Data scraping en " + url)
+            self._logger.info("Data scraping en " + url)
 
             # Algunes poques ofertes són de fotocasa i habitaclia ja indica que la major part de la info. està allí.
             # No ens interessen per manca de qualitat (dades insuficients)
             if self._fotocasa_id(url):
-                self._log("URL descartada (falta de datos)")
+                self._logger.warning("URL descartada (falta de datos)")
                 return
 
             html = self._descargar_url(url)
@@ -73,54 +64,52 @@ class HabitacliaScraper():
             # Se obtienen de "Características generales"
             antiguedad = self._get_antiguedad(parser)
             planta = self._get_planta(parser)
-            parking = 'No'
+            parking = False
             if self._general_feature_exists("Plaza parking", parser):
-                parking = 'Sí'
-            calefaccio = 'No'
+                parking = True
+            calefaccio = False
             if self._general_feature_exists("Calefacción", parser):
-                calefaccio = 'Sí'
-            aire_acondicionat = 'No'
+                calefaccio = True
+            aire_acondicionat = False
             if self._general_feature_exists("Aire acondicionado", parser):
-                aire_acondicionat = 'Sí'
-            moblat = 'Sí'
+                aire_acondicionat = True
+            moblat = True
             if self._general_feature_exists("Sin amueblar", parser):
-                moblat = 'No'
+                moblat = False
             # Etiquetat eficiència energètica i d'emissions
             eti_consum = self._get_eti_eficiencia(parser)
             eti_emissions = self._get_eti_emissions(parser)
 
             # Se obtienen de "Equipamiento comunitario"
-            jardi = 'No'
+            jardi = False
             if self._community_feature_exists("Jardín", parser):
-                jardi = 'Sí'
-            ascensor = 'No'
+                jardi = True
+            ascensor = False
             if self._community_feature_exists("Ascensor", parser):
-                ascensor = 'Sí'
+                ascensor = True
 
-            r = dict()
-            r['Id'] = housing_id
-            r['Url'] = url
-            r['Tipus'] = tipus
-            r['Municipi'] = municipi
-            r['Barri'] = barri
-            r['Preu Actual (€)'] = preuactual
-            r['Superfície (m2)'] = superficie
-            r['#Habitacions'] = habitaciones
-            r['#Banys'] = baños
-            r['Antiguitat'] = antiguedad
-            r['Planta'] = planta
-            r['Parking'] = parking
-            r['Calefacció'] = calefaccio
-            r['Aire acondicionat'] = aire_acondicionat
-            r['Moblat'] = moblat
-            r['Eficiència energètica'] = eti_consum
-            r['Classe emissions'] = eti_emissions
-            r['Jardi'] = jardi
-            r['Ascensor'] = ascensor
-            self.dataset.append(r.copy())
-            return
+            return(pd.DataFrame({'Id': housing_id,
+                                 'Url': url,
+                                 'Tipus': tipus,
+                                 'Municipi': municipi,
+                                 'Barri': barri,
+                                 'Preu Actual (€)': preuactual,
+                                 'Superfície (m2)': superficie,
+                                 '#Habitacions': habitaciones,
+                                 '#Banys': baños,
+                                 'Antiguitat': antiguedad,
+                                 'Planta': planta,
+                                 'Parking': parking,
+                                 'Calefacció': calefaccio,
+                                 'Aire acondicionat': aire_acondicionat,
+                                 'Moblat': moblat,
+                                 'Eficiència energètica': eti_consum,
+                                 'Classe emissions': eti_emissions,
+                                 'Jardi': jardi,
+                                 'Ascensor': ascensor}, index = [0]))
+            
         except Exception as ex:
-            self._log("Error en el data scraping " + str(ex))
+            self._logger.error("Error en el data scraping " + str(ex))
 
     def _fotocasa_id(self, url: str):
         url = url.replace('https://www.habitaclia.com/', '')
@@ -134,10 +123,10 @@ class HabitacliaScraper():
                 if s.lower().strip().startswith('consumo'):
                     o: str = ele.select('.rating')[0].text
                     return o.strip()
-            return '?'
+            return None
         except Exception as ex:
-            self._log("Error obteniendo la etiqueta de eficiencia energética. Se devuelve ? " + str(ex))
-            return '?'
+            self._logger.error("Error obteniendo la etiqueta de eficiencia energética. Se devuelve ? " + str(ex))
+            return None
 
     def _get_eti_emissions(self, parser: BeautifulSoup):
         try:
@@ -147,42 +136,42 @@ class HabitacliaScraper():
                 if s.lower().strip().startswith('emisiones'):
                     o: str = ele.select('.rating')[0].text
                     return o.strip()
-            return '?'
+            return None
         except Exception as ex:
-            self._log("Error obteniendo la etiqueta de emisiones. Se devuelve ? " + str(ex))
-            return '?'
+            self._logger.error("Error obteniendo la etiqueta de emisiones. Se devuelve ? " + str(ex))
+            return None
 
     def _get_antiguedad(self, parser: BeautifulSoup):
         try:
             ele = self._get_general_ele(parser)
             if not ele:
-                return '?'
+                return None
             items = ele.select('ul li')
             for item in items:
                 s = str(item.text).lower().strip()
                 if s.startswith('año construcción'):
                     planta = s.replace('año construcción', '').strip()
                     return planta
-            return '?'
+            return None
         except Exception as ex:
-            self._log("Error obteniendo la antigüedad. Se devuelve ? " + str(ex))
-            return '?'
+            self._logger.error("Error obteniendo la antigüedad. Se devuelve ? " + str(ex))
+            return None
 
     def _get_planta(self, parser: BeautifulSoup):
         try:
             ele = self._get_general_ele(parser)
             if not ele:
-                return '?'
+                return None
             items = ele.select('ul li')
             for item in items:
                 s = str(item.text).lower().strip()
                 if s.startswith('planta número'):
                     planta = s.replace('planta número', '').strip()
                     return planta
-            return '?'
+            return None
         except Exception as ex:
-            self._log("Error obteniendo la planta. Se devuelve ? " + str(ex))
-            return '?'
+            self._logger.error("Error obteniendo la planta. Se devuelve ? " + str(ex))
+            return None
 
     def _general_feature_exists(self, feature: str, parser: BeautifulSoup):
         try:
@@ -195,7 +184,7 @@ class HabitacliaScraper():
                     return True
             return False
         except Exception as ex:
-            self._log("Error obteniendo la característica general " + feature + " Se considerará que no existe " + str(ex))
+            self._logger.error("Error obteniendo la característica general " + feature + " Se considerará que no existe " + str(ex))
             return False
 
     def _community_feature_exists(self, feature: str, parser: BeautifulSoup):
@@ -209,7 +198,7 @@ class HabitacliaScraper():
                     return True
             return False
         except Exception as ex:
-            self._log("Error obteniendo la característica de comunidad " + feature +
+            self._logger.error("Error obteniendo la característica de comunidad " + feature +
                       " Se considerará que no existe " + str(ex))
             return False
 
@@ -239,7 +228,7 @@ class HabitacliaScraper():
             id = l3[len(l3) - 1]
             return id
         except Exception as ex:
-            self._log("Error obteniendo el Id de la oferta " + str(ex) + " Se aborta el scraping de esta URL")
+            self._logger.error("Error obteniendo el Id de la oferta " + str(ex) + " Se aborta el scraping de esta URL")
             raise
 
     def _get_housing_type_by_url(self, url):
@@ -256,7 +245,7 @@ class HabitacliaScraper():
                 return 'atico'
             return 'otros'
         except Exception as ex:
-            self._log("Error obteniendo el tipo de inmueble " + str(ex) + " Se aborta el scraping de esta URL")
+            self._logger.error("Error obteniendo el tipo de inmueble " + str(ex) + " Se aborta el scraping de esta URL")
             raise
 
     def _get_municipio(self, parser: BeautifulSoup):
@@ -268,8 +257,8 @@ class HabitacliaScraper():
             s = parser.select("article.location h4 a")[0].text.strip()
             return s
         except Exception as ex:
-            self._log("Error obteniendo el barrio. Se devuelve ? " + str(ex))
-            return '?'
+            self._logger.error("Error obteniendo el barrio. Se devuelve ? " + str(ex))
+            return None
 
     def _get_precio(self, parser: BeautifulSoup):
         try:
@@ -277,8 +266,8 @@ class HabitacliaScraper():
             s = s.replace('€', '').replace('.', '').strip()
             return s
         except Exception as ex:
-            self._log("Error obteniendo el precio. Se devuelve ? " + str(ex))
-            return '?'
+            self._logger.error("Error obteniendo el precio. Se devuelve ? " + str(ex))
+            return None
 
     def _get_superficie(self, html: str, parser: BeautifulSoup):
         try:
@@ -286,26 +275,28 @@ class HabitacliaScraper():
             s = s.lower().replace('desde', '').strip()
             return s
         except Exception as ex:
-            self._log("Error obteniendo la superficie. Se devuelve ? " + str(ex))
-            return '?'
+            self._logger.error("Error obteniendo la superficie. Se devuelve ? " + str(ex))
+            return None
 
     def _get_habitaciones(self, html: str, parser: BeautifulSoup):
         try:
             s = self._get_feature(html, parser, 'hab.')
             return s
         except Exception as ex:
-            self._log("Error obteniendo el número de habitaciones. Se devuelve ? " + str(ex))
-            return '?'
+            self._logger.error("Error obteniendo el número de habitaciones. Se devuelve ? " + str(ex))
+            return None
 
     def _get_baños(self, html: str, parser: BeautifulSoup):
         try:
             s = self._get_feature(html, parser, 'baños')
             if s == '?':
                 s = self._get_feature(html, parser, 'baño')
+            if s == '?':
+                s = None
             return s
         except Exception as ex:
-            self._log("Error obteniendo el número de baños. Se devuelve ? " + str(ex))
-            return '?'
+            self._logger.error("Error obteniendo el número de baños. Se devuelve ? " + str(ex))
+            return None
 
     def _get_feature(self, html: str, parser: BeautifulSoup, ends_with: str):
         features = parser.select("#js-feature-container ul li")
@@ -316,12 +307,13 @@ class HabitacliaScraper():
                 s = ss.lower().replace(ends_with.lower(), '').strip()
                 break
         if s == '':
-            s = '?'
+            s = None
         return s
 
     def _get_total_index_pages(self, html):
         parser = BeautifulSoup(html, 'html.parser')
         paginator = parser.select(".pagination ul li")
+        print(paginator)
         paginator.reverse()
         last_page_tag = paginator[2]
         last_page = last_page_tag.select("a")[0].text
@@ -331,25 +323,18 @@ class HabitacliaScraper():
         html = self._descargar_url(self.url)
         return html
 
-    def _log(self, s):
-        self._log_helper.write(s)
-
     def _descargar_url(self, url):
-        html = self._http_helper.get(url)
+        sleep(random.random() * 3)
+        response = requests.get(url)
+        # add retries if response code is not allowed
+        html = response.text
         return html
 
     def write_to_csv(self, filename):
-        self._log('Escribiendo CSV en ' + filename + '...')
-        f = open(filename, "w+")
-        hdr_written = False
-        for d in self.dataset:
-            if not hdr_written:
-                for k in d:
-                    f.write(k + ";")
-                f.write("\n")
-                hdr_written = True
-            for k in d:
-                f.write(d[k] + ";")
-            f.write("\n")
-        f.close()
-        self._log('- OK -')
+        self._logger.info('Escribiendo CSV en ' + filename + '...')
+        try:
+            self._data.to_csv(filename)
+        except Exception as ex:
+            self._logger.error("No es posible guardar los datos en un csv." + str(ex))
+            
+        self._logger.info('- OK -')
